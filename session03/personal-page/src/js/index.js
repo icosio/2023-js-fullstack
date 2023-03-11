638ccad6110991132cf259213ca0668a69415b74
MelyCosio <icosio0510@gmail.com>
2023-03-11 13:57:50 -0700

    [add] pagina personal agregda

---
+++ session03/personal-page/src/js/index.js
@@ -0,0 +1,7 @@
+import '../sass/style.scss'
+
+process.env.NODE_ENV !== 'production'
+? ()=> console.log('***Running development mode')
+:  ()=> console.log('***Running development mode')
+
+console.log('*** Personal page ');
